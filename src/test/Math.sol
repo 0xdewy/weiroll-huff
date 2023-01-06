73febd45f9bd3b6daa3314f1b2dd707d036ecd68
kyle <15720036+kyledewy@users.noreply.github.com>
2023-01-06 04:46:18 -0800

    Add functionality to save and use staticcall output

---
+++ src/test/Math.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+contract Math {
+    function add(
+        uint256 a,
+        uint256 b
+    ) public pure returns (uint) {
+        return a + b;
+    }
+}
--- test/SimpleStore.t.sol
+++ test/SimpleStore.t.sol
@@ -7,7 +7,7 @@ import "forge-std/console.sol";
 import "../src/Weiroll.sol";
 import "../src/CommandBuilder.sol";
 import "../src/test/Events.sol";
-import "../src/test/StateTest.sol";
+import "../src/test/Math.sol";
 
 contract Planner {
     // FLag values
@@ -26,6 +26,7 @@ contract Planner {
     uint8 constant END_OF_ARGS = 0xFF; // 0b11111111
     uint8 constant USE_STATE = 0xFE; // 0b11111110
 
+    // 32 byte command
     struct Command {
         address target;
         bytes4 selector;
@@ -56,7 +57,7 @@ contract Planner {
                 selector: _selector,
                 flag: bytes1(FLAG_CT_CALL),
                 inputs: new bytes1[](0),
-                out: bytes1(0)
+                out: bytes1(0xFF)
             })
         );
     }
@@ -68,7 +69,7 @@ contract Planner {
                 selector: _selector,
                 flag: bytes1(FLAG_CT_VALUECALL),
                 inputs: new bytes1[](0),
-                out: bytes1(0)
+                out: bytes1(0xFF)
             })
         );
     }
@@ -80,12 +81,13 @@ contract Planner {
                 selector: _selector,
                 flag: bytes1(FLAG_CT_DELEGATECALL),
                 inputs: new bytes1[](0),
-                out: bytes1(0)
+                out: bytes1(0xFF)
             })
         );
     }
 
-    function withArg(bytes calldata value, bool isDynamic) public {
+    // Use argument passed as calldata
+    function withRawArg(bytes calldata value, bool isDynamic) public {
         Command storage command = commands[commands.length - 1];
         if (command.inputs.length == 6) {
             revert("Planner: Too many arguments");
@@ -99,6 +101,25 @@ contract Planner {
         state.push(value);
     }
 
+    // Use argument saved from previous staticcall
+    function withArg(bytes1 index) public {
+        Command storage command = commands[commands.length - 1];
+        if (command.inputs.length == 6) {
+            revert("Planner: Too many arguments");
+        }
+        command.inputs.push(index);
+    }
+
+    // Save the output of this command to the next available state slot
+    function saveOutput() public returns (bytes1) {
+        Command storage command = commands[commands.length - 1];
+        // TODO: handle dynamic output
+        command.out = bytes1(uint8(state.length));
+        // reserve slot in state
+        state.push(new bytes(0));
+        return bytes1(uint8(state.length - 1));
+    }
+
     function encode()
         public
         returns (bytes32[] memory _commands, bytes[] memory _state)
@@ -108,15 +129,12 @@ contract Planner {
         for (uint256 i = 0; i < commands.length; i++) {
             Command storage command = commands[i];
 
-            // Fill remaining inputs with 0xFF
+            // Fill remaining inputs with 0xFF (end-of-args)
             if (command.inputs.length < 6) {
                 while (command.inputs.length < 6) {
                     command.inputs.push(bytes1(0xFF));
                 }
             }
-
-            // TODO: handle output properly
-            command.out = bytes1(0xFF);
 
             bytes memory cmd = abi.encodePacked(
                 command.selector,
@@ -158,7 +176,7 @@ contract WeirollTest is Test, Events {
     Weiroll weiroll;
     Planner planner;
     Events events;
-    StateTest stateTest;
+    Math math;
 
     // 0x<selector><flags><in0><in1><in2><in3><in4><in5><out><address>
 
@@ -168,13 +186,29 @@ contract WeirollTest is Test, Events {
         weirollHuff = IWeiroll(HuffDeployer.deploy("Weiroll"));
         planner = new Planner();
         events = new Events();
-        stateTest = new StateTest();
+        math = new Math();
+    }
+
+    // Add 2 uints and log the result in another contract
+    function testHuffAddUints() public {
+        planner.staticCall(address(math), math.add.selector);
+        planner.withRawArg(abi.encode(0x69), false);
+        planner.withRawArg(abi.encode(0x420), false);
+        bytes1 index = planner.saveOutput();
+        planner.regularCall(address(events), events.logUint.selector);
+        planner.withArg(index);
+
+        (bytes32[] memory _commands, bytes[] memory _state) = planner.encode();
+
+        vm.expectEmit(true, true, true, true);
+        emit LogUint(0x69 + 0x420);
+        weirollHuff.execute(_commands, _state);
     }
 
     function testHuffLogUint() public {
         // events.logUint(0x69);
         planner.regularCall(address(events), events.logUint.selector);
-        planner.withArg(abi.encode(0x69), false);
+        planner.withRawArg(abi.encode(0x69), false);
 
         (bytes32[] memory _commands, bytes[] memory _state) = planner.encode();
 
@@ -187,7 +221,7 @@ contract WeirollTest is Test, Events {
     function testHuffLoopLogUint() public {
         for (uint i = 0; i < 20; i++) {
             planner.regularCall(address(events), events.logUint.selector);
-            planner.withArg(abi.encode(i), false);
+            planner.withRawArg(abi.encode(i), false);
         }
 
         (bytes32[] memory _commands, bytes[] memory _state) = planner.encode();
@@ -217,7 +251,7 @@ contract WeirollTest is Test, Events {
         state[0] = abi.encode(0x20); // dest slot index
 
         planner.regularCall(address(events), events.logUint.selector);
-        planner.withArg(abi.encode(0x20), false);
+        planner.withRawArg(abi.encode(0x20), false);
 
         (bytes32[] memory _commands, bytes[] memory _state) = planner.encode();
 
